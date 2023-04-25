ae28acf2615c287dd6fab7236d02cd7443f9cd65
SHIAO SZU CHEN <allenivan@gmail.com>
2023-04-25 22:48:42 +0800

    mp0 finish

--- rust_mp_0/src/lib.rs
+++ rust_mp_0/src/lib.rs
@@ -17,7 +17,7 @@ impl Operation {
         match symbol {
             '+' => Some(Operation::Add),
             '-' => Some(Operation::Subtract),
-            '*' => Some(Operation::Multiply),
+            '*' | 'x' => Some(Operation::Multiply),
             '/' => Some(Operation::Divide),
             '%' => Some(Operation::Modulo),
             _ => None,
@@ -34,15 +34,41 @@ pub fn get_equation_tuple(line: &String) -> (Option<&str>, Option<&str>) {
 
 // [COMPLETE THIS HELPER FUNCTION]
 pub fn parse_equation(line: &String) -> Result<(f32, f32, Operation), ()> {
-    todo!();
+    let (first_num_str_result, second_num_str_result) = get_equation_tuple(line);
+
+    let first_num = first_num_str_result.ok_or_else(|| ())?.trim().parse::<f32>().map_err(|_| ())?;
+    let second_num = second_num_str_result.ok_or_else(|| ())?.trim().parse::<f32>().map_err(|_| ())?;
+
+    match line
+    .chars()
+    .find(|c| "+-x*/%".contains(*c)) 
+    {
+        None => Err(()),
+        Some(operation_char) => Ok((first_num, second_num, Operation::from_char(operation_char).ok_or_else(|| ())?))
+    }    
 }
 
 // [COMPLETE THIS FUNCTION]
 pub fn solve(equation: &String) -> Result<f32, ()> {
-    todo!();
+    let result_tuple = parse_equation(equation).map_err(|_| ())?;
+
+    match result_tuple.2 {
+        Operation::Add => Ok(result_tuple.0 + result_tuple.1),
+        Operation::Subtract => Ok(result_tuple.0 - result_tuple.1),
+        Operation::Multiply => Ok(result_tuple.0 * result_tuple.1),
+        Operation::Divide => Ok(result_tuple.0 / result_tuple.1),
+        Operation::Modulo => Ok(result_tuple.0 % result_tuple.1),
+    }
 }
 
 // [COMPLETE THIS FUNCTION]
 pub fn solve_all(file_path: &str) -> Result<f32, ()> {
-    todo!();
+    BufReader::new(File::open(file_path).map_err(|_| ())?).lines().fold(Ok(0.0), |acc, line| {
+        acc.and_then(|cur| {
+            match line {
+                Err(_) => Ok(cur),
+                Ok(line_str) => Ok(solve(&line_str).unwrap_or_default() + cur)
+            }
+        })
+    })
 }
